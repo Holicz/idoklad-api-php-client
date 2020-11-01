22d6619b3067cec4321200c33696860496966fc3
Lukáš Holeczy <info@lukasholeczy.eu>
2020-11-01 16:40:29 +0100

    Fix phpstan

--- src/Serializer/EnumHandler.php
+++ src/Serializer/EnumHandler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace DobryProgramator\iDoklad\Serializer;
 
+use InvalidArgumentException;
 use JMS\Serializer\Context;
 use JMS\Serializer\GraphNavigatorInterface;
 use JMS\Serializer\Handler\SubscribingHandlerInterface;
@@ -12,11 +13,15 @@ use JMS\Serializer\JsonSerializationVisitor;
 use MabeEnum\Enum;
 
 use function call_user_func;
+use function is_callable;
 
 final class EnumHandler implements SubscribingHandlerInterface
 {
     private const FORMAT = 'json';
 
+    /**
+     * @return array<int, array<string, int|string>>
+     */
     public static function getSubscribingMethods(): array
     {
         return [
@@ -35,15 +40,30 @@ final class EnumHandler implements SubscribingHandlerInterface
         ];
     }
 
+    /**
+     * @param array<string, mixed> $type
+     * @return mixed
+     */
     public function serializeEnumToJson(JsonSerializationVisitor $visitor, Enum $enum, array $type, Context $context)
     {
         return $enum->getValue();
     }
 
-    public function deserializeEnumFromJson(JsonDeserializationVisitor $visitor, $value, array $type, Context $context)
-    {
-        $enum = $type['params'][0];
+    /**
+     * @param mixed $value
+     * @param array<string, mixed> $type
+     */
+    public function deserializeEnumFromJson(
+        JsonDeserializationVisitor $visitor,
+        $value,
+        array $type,
+        Context $context
+    ): Enum {
+        $callable = $type['params'][0] . '::get';
+        if (!is_callable($callable)) {
+            throw new InvalidArgumentException(sprintf('"%s" is not callable.', $callable));
+        }
 
-        return call_user_func($enum . '::get', $value);
+        return call_user_func($callable, $value);
     }
 }
